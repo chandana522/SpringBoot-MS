ee9f2268dc79fd4811378eeabb20902df22673d1
[Chandana Deshpande] <chandana522@gmail.com>
2021-01-28 17:28:46 +0530

    first commit

---
+++ cloud-gateway/src/main/java/com/dailycodehandson/cloudgateway/controller/FallBackMethodController.java
@@ -0,0 +1,19 @@
+package com.dailycodehandson.cloudgateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod(){
+        return "User service is taking longer than expected"+"please try later";
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod(){
+        return "Department service is taking longer than expected"+"please try later";
+    }
+
+}
